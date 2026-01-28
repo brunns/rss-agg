903fd9467c7d13db543eebc1d991a4d64a70b3dd
Simon Brunning <simon@brunningonline.net>
2026-01-28 12:07:20 +0000

    Deployment fixes.

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,27 +50,29 @@ resource "aws_iam_role_policy_attachment" "rss_aggregator_lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_lambda_permission" "api_gateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.rss_aggregator.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.rss_aggregator_flask_api.execution_arn}/*/*"
+}
+
 resource "aws_api_gateway_rest_api" "rss_aggregator_flask_api" {
   name        = "Flask API"
   description = "API Gateway for Flask application"
 }
 
-resource "aws_api_gateway_resource" "rss_aggregator_root" {
-  rest_api_id = aws_api_gateway_rest_api.rss_aggregator_flask_api.id
-  parent_id   = aws_api_gateway_rest_api.rss_aggregator_flask_api.root_resource_id
-  path_part   = ""
-}
-
 resource "aws_api_gateway_method" "rss_aggregator_root_method" {
   rest_api_id   = aws_api_gateway_rest_api.rss_aggregator_flask_api.id
-  resource_id   = aws_api_gateway_resource.rss_aggregator_root.id
+  resource_id   = aws_api_gateway_rest_api.rss_aggregator_flask_api.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "rss_aggregator_root_integration" {
   rest_api_id             = aws_api_gateway_rest_api.rss_aggregator_flask_api.id
-  resource_id             = aws_api_gateway_resource.rss_aggregator_root.id
+  resource_id             = aws_api_gateway_rest_api.rss_aggregator_flask_api.root_resource_id
   http_method             = aws_api_gateway_method.rss_aggregator_root_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -100,14 +102,29 @@ resource "aws_api_gateway_integration" "rss_aggregator_lambda_integration" {
 }
 
 resource "aws_api_gateway_deployment" "rss_aggregator_flask_api_deployment" {
-  depends_on = [
-    aws_api_gateway_integration.rss_aggregator_lambda_integration,
-    aws_api_gateway_integration.rss_aggregator_root_integration
-  ]
   rest_api_id = aws_api_gateway_rest_api.rss_aggregator_flask_api.id
-  stage_name  = "prod"
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_method.rss_aggregator_root_method.id,
+      aws_api_gateway_integration.rss_aggregator_root_integration.id,
+      aws_api_gateway_resource.rss_aggregator_proxy.id,
+      aws_api_gateway_method.rss_aggregator_proxy_method.id,
+      aws_api_gateway_integration.rss_aggregator_lambda_integration.id
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.rss_aggregator_flask_api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.rss_aggregator_flask_api.id
+  stage_name    = "prod"
 }
 
 output "api_url" {
-  value = aws_api_gateway_deployment.rss_aggregator_flask_api_deployment.invoke_url
+  value = aws_api_gateway_stage.prod.invoke_url
 }
